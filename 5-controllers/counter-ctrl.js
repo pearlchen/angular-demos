12c40b09fabb45bd1cc7bd0d6af45f58cf2c89f3
Pearl Chen <pearl@klab.ca>
2014-03-27 20:46:14 -0400

    Added CSS transition and updated colour.

--- 5-controllers/counter-ctrl.js
+++ 5-controllers/counter-ctrl.js
@@ -1,15 +1,17 @@
 function CounterCtrl($scope) {
 
   $scope.counter = 0;
-  $scope.jumbotronStyle = {'background-color': 'rgba(223,240,216,0)'};
+  $scope.happyColor = '92,184,92'; // a bright green
+  $scope.jumbotronStyle = {'background-color': 'rgba('+$scope.happyColor+',0)'};
 
   $scope.incrementCounter = function() {
     $scope.counter++; 
-    $scope.jumbotronStyle = {'background-color': 'rgba(223,240,216,'+$scope.counter/20+')'}
+    $scope.jumbotronStyle = {'background-color': 'rgba('+$scope.happyColor+','+$scope.counter/20+')'}
   };
+  
   $scope.resetCounter = function(){
     $scope.counter = 0;
-    $scope.jumbotronStyle = {'background-color': 'rgba(223,240,216,0)'};
+    $scope.jumbotronStyle = {'background-color': 'rgba('+$scope.happyColor+',0)'};
   };
 
 }
