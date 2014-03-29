eb3e05114926d5c30c48adebbb8195853c81ce9d
Pearl Chen <pearl@klab.ca>
2014-03-29 16:15:04 -0400

    Stripped out comments so it's easier to compare.

--- 7-rewrite/after/scripts.js
+++ 7-rewrite/after/scripts.js
@@ -1,6 +1,3 @@
-/* This demo was created to demonstrate async call stack debugging in Chrome DevTools. */
-/* This demo is best viewed in Chrome Canary. */
-
 function MailCtrl($scope, $timeout, $http) {
 
   $scope.timesSubmitted = 0;
@@ -11,68 +8,37 @@ function MailCtrl($scope, $timeout, $http) {
   $scope.isSending = false;
 
   $scope.submitHandler = function() {
-    
-    // disable submit button to avoid double-clicks
     $scope.isSending = true;
-
-    // try submitting the form data
     $scope.timesSubmitted = 0;
     $scope.submitData();
-
   };
 
   $scope.submitData = function() {
-
-    // increate the counter for submission tries and then make a post request
     $scope.timesSubmitted++;
     $http.post("fakeapi").success($scope.postOnSuccess).error($scope.postOnFail);
-  }
+  };
 
   $scope.postOnSuccess = function(data) {
-
-    // the post was successful!
-    console.log("post success");
-
-  } 
+  };
 
   $scope.postOnFail = function(data) {
-
-    // the post failed
-    console.log("post fail");
-    
     if ( $scope.timesSubmitted < $scope.maxTries ) {
-
-      // maybe it's me, not you
       $scope.retrySubmit();
-
     }
     else {
-      
-      console.log("failed");
-
-      // no more retries, show error :(
       $scope.isRetrying = false;
       $scope.isFailed = true;
-      
-      // re-enable submit button
       $scope.isSending = false;
-
     }
-
-  } 
+  };
 
   $scope.dismissAlert = function() {
     $scope.isFailed = false;
-  }
+  };
 
   $scope.retrySubmit = function() {
-
-    // show retry message
     $scope.isRetrying = true;
-
-    // wait to call submitData() again
     $timeout( $scope.submitData, 1000 );
-
-  }
+  };
 
 }
